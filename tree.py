64942d9efd473171e9df812536bd60d46c45690f
Matei Costescu <plapusk.git@gmail.com>
2024-04-22 16:49:52 +0300

    tree.py

--- tree.py
+++ tree.py
@@ -62,27 +62,57 @@ class Tree:
             return self._find(data, node.right)
 
     def deleteTree(self):
-        # TODO 1
+        """ Delete the entire tree by setting the root to None """
         self.root = None
 
-    def printTree(self):
-        # TODO 1
-        if self.root is not None:
-            self._printInorderTree(self.root)
+def printTree(self):
+    """ Print the tree using inorder traversal """
+    if self.root is not None:
+        self._printInorderTree(self.root)
 
-    def _printInorderTree(self, node):
-        # TODO 1
-        if node is not None:
-            self._printInorderTree(node.left)
-            print(str(node.data) + ' ')
-            self._printInorderTree(node.right)
+def _printInorderTree(self, node):
+    """ Print the subtree rooted at the given node using inorder traversal """
+    if node is not None:
+        self._printInorderTree(node.left)
+        print(str(node.data) + ' ')
+        self._printInorderTree(node.right)
 
     def _printPreorderTree(self, node):
-        # TODO 2
-        pass
+        """ Print the subtree rooted at the given node using preorder traversal """
+        if node is not None:
+            print(str(node.data) + ' ')
+            self._printPreorderTree(node.left)
+            self._printPreorderTree(node.right)
 
-    def _printPostorderTree(self, node):
-        # TODO 2
-        pass
+def _printPostorderTree(self, node):
+    """ Print the subtree rooted at the given node using postorder traversal """
+    if node is not None:
+        self._printPostorderTree(node.left)
+        self._printPostorderTree(node.right)
+        print(str(node.data) + ' ')
 
+import unittest
 
+class TestTreeMethods(unittest.TestCase):
+    def setUp(self):
+        self.tree = Tree()
+        self.tree.add(5)
+        self.tree.add(3)
+        self.tree.add(7)
+        self.tree.add(2)
+        self.tree.add(4)
+
+    def test_find_existing(self):
+        node = self.tree.find(3)
+        self.assertEqual(node.data, 3)
+
+    def test_find_nonexistent(self):
+        node = self.tree.find(6)
+        self.assertIsNone(node)
+
+    def test_find_root(self):
+        node = self.tree.find(5)
+        self.assertEqual(node.data, 5)
+
+if __name__ == '__main__':
+    unittest.main()
